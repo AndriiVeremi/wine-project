43559c31af337020787e74e0559663c3634821df
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2026-02-22 00:26:51 +0100

    Revert "add delete_review controller & finish locationRouter"
    This reverts commit cbdf317c2abe675161e526cd90025c89d6a81c3e.

--- backend/src/controllers/locationController.ts
+++ backend/src/controllers/locationController.ts
@@ -12,53 +12,16 @@ class LocationController {
     }
   }
 
-  public async getCountryById(req: Request, res: Response, next: NextFunction) {
-    try {
-      const countryId = req.params.id as string || req.query.country as string;
-      const country = await LocationService.getCountryById(countryId);
-
-      console.log('Country ID:', countryId);
-      console.log('Country found:', country);
-
-      if (!country) {
-        throw new HttpError('Country not found', 404);
-      }
-      res.json(country);
-    } catch (error) {
-      next(error);
-    }
-  } 
-
   public async getRegionsByCountry(req: Request, res: Response, next: NextFunction) {
     try {
-      const { countryId } = req.params;
+      const { country } = req.query;
 
-      if (!countryId) {
-        throw new HttpError('Country name parameter is required', 400);
+      if (!country) {
+        throw new HttpError('Country query parameter is required', 400);
       }
 
-      const regions = await LocationService.getRegionsByCountry(countryId as string);
+      const regions = await LocationService.getRegionsByCountry(country as string);
       res.json(regions);
-    } catch (error) {
-      next(error);
-    }
-  }
-
-  public async getRegionsById(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { countryId, regionId } = req.params;
-
-      if (!countryId || !regionId) {
-        throw new HttpError('Country ID and Region ID parameters are required', 400);
-      }
-
-      const region = await LocationService.getRegionById(countryId as string, regionId as string);
-
-      if (!region) {
-        throw new HttpError('Region not found', 404);
-      }
-      res.json(region);
-
     } catch (error) {
       next(error);
     }
