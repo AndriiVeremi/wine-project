8437aca0e0875bbdde7c466491161bda5377ce9d
Andrii Popov <97399846+Andrii0207@users.noreply.github.com>
2026-03-05 01:50:47 +0200

    finish footer

--- frontend/src/components/Footer/Footer.styled.ts
+++ frontend/src/components/Footer/Footer.styled.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { FaFacebookF, FaTelegramPlane } from 'react-icons/fa';
+import { BiLogoInstagramAlt } from 'react-icons/bi';
 import '@/styles/vars.css';
 
 export const StyledFooter = styled('footer')`
@@ -9,8 +11,12 @@ export const FooterContainer = styled('div')`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 30px 0;
+  padding-top: 50px;
 `;
+
+export const LogoWrapper = styled('div')`
+  padding: 22px 0 22px 0;
+`
 
 export const InfoList = styled('ul')`
   display: flex;
@@ -19,5 +25,60 @@ export const InfoList = styled('ul')`
 
 export const SocialList = styled('ul')`
   display: flex;
-  gap: 30px;
+  gap: 16px;
+`;
+
+export const Text = styled('p')`
+  font-family: var(--font-main);
+  font-size: 19px;
+  font-weight: 500;
+  color: var(--font-grey);
+  line-height: 26px;
+`;
+
+export const FacebookIcon = styled(FaFacebookF)`
+  color: var(--primary-wine);
+  width: 16px;
+  height: 28px;
+`;
+
+export const InstagramIcon = styled(BiLogoInstagramAlt)`
+  color: var(--primary-wine);
+  width: 32px;
+  height: 32px;
+`;
+export const TelegramIcon = styled(FaTelegramPlane)`
+  color: var(--primary-wine);
+  width: 30px;
+  height: 30px;
+`;
+
+export const IconCircle = styled('div')`
+  width: 52px;
+  height: 52px;
+  border: 1px solid var(--primary-wine);
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 1s ease;
+
+  &:hover {
+    background-color: var(--primary-wine);
+    ${FacebookIcon}, ${InstagramIcon}, ${TelegramIcon} {
+      color: var(--footer-cream);
+      transition: all 0.5s ease;
+    }
+  }
+`;
+
+export const Copyright = styled('p')`
+  font-family: var(--font-main);
+  font-size: 16px;
+  font-weight: 400;
+  font-style: regular;
+  color: var(--font-grey);
+  line-height: 20px;
+  text-align: center;
+  padding-bottom: 32px;
 `;
