294256a4c9f6f1cd27a8ae717af1c7fff5b894a0
Andrii Popov <97399846+Andrii0207@users.noreply.github.com>
2026-03-22 23:29:43 +0200

    Update InteractiveMap.tsx

--- frontend/src/components/Common/InteractiveMap/InteractiveMap.tsx
+++ frontend/src/components/Common/InteractiveMap/InteractiveMap.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MapWrapper, Tooltip } from './InteractiveMap.styled';
 import RegionPath from './RegionPath';
